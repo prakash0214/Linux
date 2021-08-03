86a6df8fe874d8b6c487348dad49be02860331e1
prakash0214 <77110798+prakash0214@users.noreply.github.com>
2021-08-03 11:20:39 +0530

    sumofdigits.c

---
+++ sumofdigits.c
@@ -0,0 +1,18 @@
+// w a p to sumogdigits ?
+
+#include<stdio.h>
+int main()
+{
+  int num,i=0,rem=0,sum=0;
+  
+  printf("enter the number:");
+  scanf("%d",&num);
+  
+  while(num)
+  {
+    rem=num%10;
+    sum=sum+rem;
+    num=num/10;
+  }
+  printf("sum=%d",sum);
+}
